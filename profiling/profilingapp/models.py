a5142e9a5f68cede71813657ae2f42cc4efb4289
rakinplaban <rakinshahriar54@gmail.com>
2024-09-10 11:45:19 +0600

    Create successful!

--- profiling/profilingapp/models.py
+++ profiling/profilingapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    father_name = models.CharField(max_length=100)
+    address = models.TextField()
+    photo = models.ImageField(upload_to='photos/')
+
+    def __str__(self):
+        return self.name
